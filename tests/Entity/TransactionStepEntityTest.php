a44c0747036e409a90418221b4f70d3bd9713495
Izarrualde <diego.r.izarrualde@gmail.com>
2021-05-03 17:32:19 -0300

    test(TransactionEntityTest, TransactionStepEntityTest, SharedDataTest): add tests cases

---
+++ tests/Entity/TransactionStepEntityTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BambooPaymentTests\Entity;
+
+use BambooPayment\Entity\TransactionStep;
+use BambooPaymentTests\SharedDataTest;
+
+/**
+ * Class TransactionStepEntityTest
+ * @package BambooPaymentTests\Entity
+ */
+class TransactionStepEntityTest extends SharedDataTest
+{
+    protected const STEP               = 'Step';
+    protected const CREATED            = 'Created';
+    protected const STATUS             = 'Status';
+    protected const RESPONSE_CODE      = 'ResponseCode';
+    protected const RESPONSE_MESSAGE   = 'ResponseMessage';
+    protected const ERROR              = 'Error';
+    protected const AUTHORIZATION_CODE = 'AuthorizationCode';
+    protected const UNIQUE_ID          = 'UniqueId';
+
+    public function testHydrate(): void
+    {
+        $transactionStep = new TransactionStep();
+        $data            = $this->getDataOfTransactionStep();
+        $transactionStep = $transactionStep->hydrate($data);
+
+        self::assertEquals($data[self::STEP], $transactionStep->getStep());
+        self::assertEquals($data[self::CREATED], $transactionStep->getCreated());
+        self::assertEquals($data[self::STATUS], $transactionStep->getStatus());
+        self::assertEquals($data[self::RESPONSE_CODE], $transactionStep->getResponseCode());
+        self::assertEquals($data[self::RESPONSE_MESSAGE], $transactionStep->getResponseMessage());
+        self::assertEquals($data[self::ERROR], $transactionStep->getError());
+        self::assertEquals($data[self::AUTHORIZATION_CODE], $transactionStep->getAuthorizationCode());
+        self::assertEquals($data[self::UNIQUE_ID], $transactionStep->getUniqueId());
+    }
+}
---
+++ tests/SharedDataTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BambooPaymentTests;
+
+
+use BambooPaymentTests\Entity\TransactionStepEntityTest;
+
+/**
+ * Class SharedDataTest
+ * @package BambooPaymentTests
+ */
+class SharedDataTest extends BaseTest
+{
+    protected function getDataOfTransactionStep(): array
+    {
+        return [
+            TransactionStepEntityTest::STEP               => 'FirstData Authorization with CVV',
+            TransactionStepEntityTest::CREATED            => '2021-05-03T15:22:22.370',
+            TransactionStepEntityTest::STATUS             => 'Approved',
+            TransactionStepEntityTest::RESPONSE_CODE      => '0',
+            TransactionStepEntityTest::RESPONSE_MESSAGE   => '00',
+            TransactionStepEntityTest::ERROR              => '',
+            TransactionStepEntityTest::AUTHORIZATION_CODE => '123456',
+            TransactionStepEntityTest::UNIQUE_ID          => null
+        ];
+    }
+}
