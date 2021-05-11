7f128a32fe7e6242fe412923b9b5e6edcefe3b74
Izarrualde <diego.r.izarrualde@gmail.com>
2021-05-11 15:26:57 -0300

    test(ShareData, AccountEntityTest, TokenInfoEntityTest): add tests

--- tests/SharedData.php
+++ tests/SharedData.php
@@ -3,6 +3,9 @@
 namespace BambooPaymentTests;
 
 
+use BambooPayment\Entity\Address;
+use BambooPayment\Entity\TransactionStep;
+use BambooPaymentTests\Entity\AddressEntityTest;
 use BambooPaymentTests\Entity\TransactionStepEntityTest;
 
 /**
@@ -11,6 +14,9 @@ use BambooPaymentTests\Entity\TransactionStepEntityTest;
  */
 class SharedData extends BaseTest
 {
+    /**
+     * @return array
+     */
     protected function getDataOfTransactionStep(): array
     {
         return [
@@ -23,5 +29,49 @@ class SharedData extends BaseTest
             TransactionStepEntityTest::AUTHORIZATION_CODE => '123456',
             TransactionStepEntityTest::UNIQUE_ID          => null
         ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function getDataOfAddress(): array
+    {
+        return [
+            'AddressId'     => 51615,
+            'AddressType'   => 1,
+            'Country'       => 'UY',
+            'State'         => 'Montevideo',
+            'AddressDetail' => '10000',
+            'PostalCode'    => null,
+            'City'          => 'MONTEVIDEO'
+        ];
+    }
+
+    /**
+     * @param array           $transactionStepData
+     * @param TransactionStep $transactionStep
+     */
+    protected function makeTestOfTransactionStep(array $transactionStepData, $transactionStep): void
+    {
+        /* @var TransactionStep $transactionStep */
+        self::assertEquals($transactionStepData[TransactionStepEntityTest::STEP], $transactionStep->getStep());
+        self::assertEquals($transactionStepData[TransactionStepEntityTest::CREATED], $transactionStep->getCreated());
+        self::assertEquals($transactionStepData[TransactionStepEntityTest::STATUS], $transactionStep->getStatus());
+        self::assertEquals($transactionStepData[TransactionStepEntityTest::RESPONSE_CODE], $transactionStep->getResponseCode());
+        self::assertEquals($transactionStepData[TransactionStepEntityTest::RESPONSE_MESSAGE], $transactionStep->getResponseMessage());
+        self::assertEquals($transactionStepData[TransactionStepEntityTest::ERROR], $transactionStep->getError());
+        self::assertEquals($transactionStepData[TransactionStepEntityTest::AUTHORIZATION_CODE], $transactionStep->getAuthorizationCode());
+        self::assertEquals($transactionStepData[TransactionStepEntityTest::UNIQUE_ID], $transactionStep->getUniqueId());
+    }
+
+    protected function makeTestOfAddress(array $data, Address $address): void
+    {
+        self::assertEquals($data[AddressEntityTest::ADDRESS_ID], $address->getAddressId());
+        self::assertEquals($data[AddressEntityTest::ADDRESS_TYPE], $address->getAddressType());
+        self::assertEquals($data[AddressEntityTest::COUNTRY], $address->getCountry());
+        self::assertEquals($data[AddressEntityTest::STATE], $address->getState());
+        self::assertEquals($data[AddressEntityTest::ADDRESS_DETAIL], $address->getAddressDetail());
+        self::assertEquals($data[AddressEntityTest::CITY], $address->getCity());
+        self::assertNull($address->getPostalCode());
     }
 }
